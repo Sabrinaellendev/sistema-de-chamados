cb9d05fab75a6f0e0ef12478652730d647b796d0
Sabrina Ellen <sabrina000ellen@gmail.com>
2026-07-26 01:07:26 -0300

    melhoria de interface do sistema

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
+const chamados = [];
 const formulario = document.querySelector("#formChamado");
+const listaChamados = document.querySelector("#listaChamados");
 
 formulario.addEventListener("submit", function (event) {
+
+
     event.preventDefault();
+
+    const solicitante = document.querySelector("#solicitante").value;
+    const setor = document.querySelector("#setor").value;
+    const categoria = document.querySelector("#categoria").value;
+    const assunto = document.querySelector("#assunto").value;
+    const descricao = document.querySelector("#descricao").value;
+
+    const chamado = {
+        solicitante: solicitante,
+        setor: setor,
+        categoria: categoria,
+        assunto: assunto,
+        descricao: descricao
+    };
+
+    chamados.push(chamado);
+
+    exibirChamados();
 
     alert("Chamado enviado com sucesso!");
 
-    const solicitante = document.querySelector("#solicitante").value;
-
-    console.log(solicitante);
 });
+
+function exibirChamados() {
+
+    listaChamados.innerHTML = "";
+
+    for (const chamado of chamados) {
+
+        listaChamados.innerHTML += `
+     <div class="card">
+
+     <h3>${chamado.assunto}</h3>
+     <p><strong>Solicitante:</strong> ${chamado.solicitante}</p>
+     <p><strong>Setor:</strong> ${chamado.setor}</p>
+     
+     </div>
+
+     `;
+
+    }
+
+};
